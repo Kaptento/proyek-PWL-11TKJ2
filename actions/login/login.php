f9b5bb9755380278b4ec70add483caf536d758f4
Kaptento <louisgracia456@gmail.com>
2025-10-09 13:54:26 +0700

    wip: implement login page

---
+++ actions/login/login.php
@@ -0,0 +1,18 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = ""; // Kosongkan jika tidak ada password
+$database = "nama_database";
+
+// Membuat koneksi
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Memeriksa koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+echo "Koneksi berhasil!";
+
+// Menutup koneksi (opsional, biasanya dilakukan di akhir skrip)
+// mysqli_close($conn);
+?>
