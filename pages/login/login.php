3993b33227856418ad8e1d67ba8f00283d690c3e
Kaptento <louisgracia456@gmail.com>
2025-10-23 13:52:43 +0700

    feat:implement login page

--- pages/login/login.php
+++ pages/login/login.php
@@ -26,7 +26,7 @@
 
     <!-- Format Buat Login -->
    <div class="container">
-    <form action="../../Homepage.html" method="POST">
+    <form method="POST">
          <div class="form-group">
             <label for="">Name</label>
             <input type="text" name="name" class="form-control" required>
