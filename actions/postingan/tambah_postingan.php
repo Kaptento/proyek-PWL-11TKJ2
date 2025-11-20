ec20bf18ae85cb4f05d9723b8ef7980b6399df42
Kaptento <louisgracia456@gmail.com>
2025-11-20 13:18:31 +0700

    wip: fix postingan page

--- actions/postingan/tambah_postingan.php
+++ actions/postingan/tambah_postingan.php
@@ -1,26 +1,27 @@
 <?php
 session_start();
 
-$user_id = $_SESSION['user']['id'];
 
 require_once "../../config/db-connection.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
+    
     $title   = mysqli_real_escape_string($connection, $_POST['title']);
     $content = mysqli_real_escape_string($connection, $_POST['content']);
-
+    
     // Default jika tidak ada gambar
+    $user_id = $_SESSION['user']['id'];
     $image_path = null;
 
     // Upload gambar
     if(isset($_FILES['gambar']) && $_FILES['gambar']['error'] == 0){
-        $target_dir = "../../foto-users/";
+        $target_dir = "../../images/posts/";
         $image_name = time() . "_" . basename($_FILES["gambar"]["name"]); // biar unik
         $target_file = $target_dir . $image_name;
 
+
         if(move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file)){
-            $image_path = "foto-users/" . $image_name;  
+            $image_path = "images/posts/" . $image_name;  
         }
     }
 
--- actions/postingan/update_postingan.php
+++ actions/postingan/update_postingan.php
@@ -11,12 +11,12 @@ $oldData = mysqli_fetch_assoc($old);
 
 // Jika gambar baru diupload
 if(isset($_FILES['gambar']) && $_FILES['gambar']['error'] == 0){
-    $target_dir = "../../foto-users/";
+    $target_dir = "../../images/posts/";
     $image_name = time() . "_" . basename($_FILES["gambar"]["name"]);
     $target_file = $target_dir . $image_name;
 
     if(move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file)){
-        $image_path = "foto-users/" . $image_name;
+        $image_path = "images/posts/" . $image_name;
 
         // Hapus gambar lama jika ada
         if(!empty($oldData['image_path']) && file_exists("../../" . $oldData['image_path'])){
